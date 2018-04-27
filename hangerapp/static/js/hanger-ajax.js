91efdfeec1083436f0228b28f75b372dd268ee0c
Vadim Zherdev <vadim.zherdev@gmail.com>
2018-04-27 22:22:31 +0300

    First working version (after correcting the URLs in AJAX)

--- hangerapp/static/js/hanger-ajax.js
+++ hangerapp/static/js/hanger-ajax.js
@@ -1,5 +1,5 @@
 $('#reset').click(function(){
-    $.get('/hanger/async', {guess_letter: '*'}, function(data){
+    $.get('/async', {guess_letter: '*'}, function(data){
         $('#status_msg').html(data['message']);
         $('#hint').html(data['hint'])
         $('#used_ltrs').html(data['used'])
@@ -11,7 +11,7 @@ $('#reset').click(function(){
 });
 
 $('#wrong_attempts').click(function(){
-   $.get('/hanger/asyncret', 0, function(data){
+   $.get('/asyncret', 0, function(data){
         $('#debug_txt').html(data['secret']);
     });
 });
@@ -20,7 +20,7 @@ $(document).keypress(function(e) {
   //alert( "Handler for .keydown() called." );
    letter = String.fromCharCode(e.which)
    //console.log(letter)
-   $.get('/hanger/async', {guess_letter: letter}, function(data){
+   $.get('/async', {guess_letter: letter}, function(data){
        $('#status_msg').html(data['message']);
        $('#hint').html(data['hint'])
        $('#used_ltrs').html(data['used'])
